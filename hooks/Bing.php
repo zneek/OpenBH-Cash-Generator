3cd2468aaa0a4b39ea88c139b8c5185ee2a7c738
neek <neek@syndk8.com>
2010-10-04 10:26:33 +0200

    bing..

--- hooks/Bing.php
+++ hooks/Bing.php
@@ -28,7 +28,7 @@ class Bing implements HookBase
 	function EnrichContent($content,$keyword,$args)
 	{
                 $kw = str_replace(" ","+",$keyword);
-		$url = "http://www.bing.com/search?q=$kw+language%3Aen&form=QBRE&filt=all&qb=4&count=50";
+                $url = sprintf("http://www.bing.com/search?q=%s&language=en&form=QBRE&filt=all&qb=4&count=50",$kw);
 		preg_match_all("/<h3><a (.+?)<\/p/si",Internet::Grab($url),$cmatches);
 		$gContent = implode(" ",$cmatches[1]);
 		$gContent = preg_replace("/(<\/?)(\w+)([^>]*>)/e"," ",$gContent);
